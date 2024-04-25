c35e975641b8aae87af63c4184eaf00fe4df8f6e
alfoval26 <148823515+alfoval26@users.noreply.github.com>
2024-04-25 23:13:44 +0000

    tarea api starwar

--- src/app.py
+++ src/app.py
@@ -8,8 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Character, Planet, Favorite
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,14 +35,216 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/user', methods=['GET', "POST"])
+def handle_user():
+    if request.method == 'GET':
+        users = User.query.all()
+        users = list(map(lambda user: user.to_dict(), users))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+        return jsonify({
+            "data": users
+        }), 200
+    elif request.method == 'POST':
+        user = User()
+        data = request.get_json()
+        user.name = data["name"]
+        user.username = data["username"]
+        user.password = data["password"]
 
-    return jsonify(response_body), 200
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({
+            "msg": "user created"
+        }), 200
+
+@app.route("/user/<int:id>", methods=["GET","PUT", "DELETE"])
+def update_user(id):
+    if request.method == 'GET':
+        user_id = id
+        user = User.query.get(id)
+        data = user.to_dict()
+
+        return data, 200
+    elif request.method == 'PUT':
+        user = User.query.get(id)
+        if user is not None:
+            data = request.get_json()
+            user.name = data["name"]
+            user.username = data["username"]
+            db.session.commit()
+            return jsonify({
+                "msg":"user updated"
+            }),200
+        else:
+            return jsonify({
+                "msg": "user not found"
+            }), 404
+    elif request.method == 'DELETE':
+        user = User.query.get(id)
+        if user is not None:
+            db.session.delete(user)
+            db.session.commit()
+
+            return jsonify({
+                "msg":"user deleted"
+            }),202
+        else:
+            return jsonify({
+                "msg":"user not found"
+            }), 404
+
+@app.route('/character', methods=['GET', "POST"])
+def handle_character():
+    if request.method == 'GET':
+        characters = Character.query.all()
+        characters = list(map(lambda character: character.to_dict(), characters))
+
+        return jsonify({
+            "data": characters
+        }), 200
+    elif request.method == 'POST':
+        character = Character()
+        data = request.get_json()
+        character.name = data["name"]
+
+        db.session.add(character)
+        db.session.commit()
+
+        return jsonify({
+            "msg": "character created"
+        }), 200
+
+@app.route("/character/<int:id>", methods=["PUT", "DELETE"])
+def update_character(id):
+    if request.method == 'PUT':
+        character = Character.query.get(id)
+        if character is not None:
+            data = request.get_json()
+            character.name = data["name"]
+            db.session.commit()
+            return jsonify({
+                "msg":"character updated"
+            }),200
+        else:
+            return jsonify({
+                "msg": "character not found"
+            }), 404
+    elif request.method == 'DELETE':
+        character = Character.query.get(id)
+        if character is not None:
+            db.session.delete(character)
+            db.session.commit()
+
+            return jsonify({
+                "msg":"character deleted"
+            }),202
+        else:
+            return jsonify({
+                "msg":"character not found"
+            }), 404
+
+@app.route('/planet', methods=['GET', "POST"])
+def handle_planet():
+    if request.method == 'GET':
+        planets = Planet.query.all()
+        planets = list(map(lambda planet: planet.to_dict(), planets))
+
+        return jsonify({
+            "data": planets
+        }), 200
+    elif request.method == 'POST':
+        planet = Planet()
+        data = request.get_json()
+        planet.name = data["name"]
+
+        db.session.add(planet)
+        db.session.commit()
+
+        return jsonify({
+            "msg": "planet created"
+        }), 200
+
+@app.route("/planet/<int:id>", methods=["PUT", "DELETE"])
+def update_planet(id):
+    if request.method == 'PUT':
+        planet = Planet.query.get(id)
+        if planet is not None:
+            data = request.get_json()
+            planet.name = data["name"]
+            db.session.commit()
+            return jsonify({
+                "msg":"planet updated"
+            }),200
+        else:
+            return jsonify({
+                "msg": "planet not found"
+            }), 404
+    elif request.method == 'DELETE':
+        planet = Planet.query.get(id)
+        if planet is not None:
+            db.session.delete(planet)
+            db.session.commit()
+
+            return jsonify({
+                "msg":"planet deleted"
+            }),202
+        else:
+            return jsonify({
+                "msg":"planet not found"
+            }), 404
+
+@app.route('/user/<int:id>/favorite', methods=['GET'])
+def handle_favorite(id):
+    user_id = id
+    favorites = Favorite.query.filter_by(user_id=user_id)
+    favorites = list(map(lambda favorite: favorite.to_dict(), favorites))
+
+    return jsonify({
+        "data": favorites
+    }), 200
+    
+@app.route('/favorite', methods=["POST"])
+def create_favorite():
+    favorite = Favorite()
+    data = request.get_json()
+    user_id = data["user_id"]
+    if data["character_id"] is None:
+        character_id = "0"
+    character_id = data["character_id"]
+    if data["planet_id"] is None:
+        planet_id = "0"
+    planet_id = data["planet_id"]
+    
+
+    user_filter = User.query.filter_by(id=user_id).first()
+    character_filter = Character.query.filter_by(id=character_id).first()
+    planet_filter = Planet.query.filter_by(id=planet_id).first()
+
+    if user_filter is not None and character_filter is not None:
+        favorite.user_id = data["user_id"]
+        favorite.user_character = data["character_id"]
+        db.session.add(favorite)
+        db.session.commit()
+
+        return jsonify({
+        "msg": "favorite created"
+        }), 200
+
+    elif user_filter is not None and planet_filter is not None:
+        favorite.user_id = data["user_id"]
+        favorite.user_planet = data["planet_id"]
+        db.session.add(favorite)
+        db.session.commit()
+
+        return jsonify({
+        "msg": "favorite created"
+        }), 200
+
+    else:
+        return jsonify({
+                "msg":"favorite could not be create, make sure user, character or planet exists"
+            }), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
